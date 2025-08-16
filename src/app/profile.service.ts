7c4b271bc903e434364dc4a3aaabef88d480e07d
Vidovic Branislav <branislav.vidovic@ext.a1.at>
2025-08-16 17:13:31 +0200

    shadows, more spacing, all projects included

--- src/app/profile.service.ts
+++ src/app/profile.service.ts
@@ -32,6 +32,54 @@ export class ProfileService {
           text: 'GitHub',
           url: 'https://github.com/baizba/jumper'
         }
+      },
+      {
+        title: 'Camunda Developer and Architect',
+        description: 'Developing BPMN Workflows and Consulting for mayor telco in Austria',
+        link: {
+          text: 'Details',
+          url: 'https://camunda.com/'
+        }
+      },
+      {
+        title: 'Camunda Developer and Consultant',
+        description: 'Developing BPMN Workflows and Consulting for mayor telco in Austria',
+        link: {
+          text: 'Details',
+          url: 'https://camunda.com/'
+        }
+      },
+      {
+        title: 'E-Commerce SAP-CX',
+        description: 'Consulting the major construction company in Austria and acting as an architect and liaison for client.',
+        link: {
+          text: 'Details',
+          url: 'https://sap.com/'
+        }
+      },
+      {
+        title: 'E-Commerce SAP-CX',
+        description: 'Developer and team lead for a world mayor installations company.',
+        link: {
+          text: 'Details',
+          url: 'https://sap.com/'
+        }
+      },
+      {
+        title: 'E-Commerce SAP-CX',
+        description: 'Developer for a mayor E-Commerce client in Switzerland',
+        link: {
+          text: 'Details',
+          url: 'https://sap.com/'
+        }
+      },
+      {
+        title: 'OpenCMS Insurance Portal',
+        description: 'Develop unified OpenCMS platform for all mayor insurance clients in Germany',
+        link: {
+          text: 'Details',
+          url: 'https://sap.com/'
+        }
       }
     ]
   }
